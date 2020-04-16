2679f26eb590739d5e12d1f04fa6b8a1e1b8c027
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-04-16 12:36:38 +0200

    Removed test code.

--- RestAPISCS/DatabaseUtility/PlaceHolder.cs
+++ RestAPISCS/DatabaseUtility/PlaceHolder.cs
@@ -8,9 +8,6 @@ namespace RestAPISCS.DatabaseUtility
 {
     public class PlaceHolder
     {
-        private void Test()
-        {
-            DataBases.Access<object>("test", "testTable").Delete(new Dictionary<string, object>());
-        }
+        
     }
 }
