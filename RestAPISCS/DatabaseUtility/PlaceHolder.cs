330106923f71b605d08dd6d739201600bfe41d42
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-04-16 11:32:43 +0200

    Added small sample of outside solution reference code.

--- RestAPISCS/DatabaseUtility/PlaceHolder.cs
+++ RestAPISCS/DatabaseUtility/PlaceHolder.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DBUtility;
 
 namespace RestAPISCS.DatabaseUtility
 {
     public class PlaceHolder
     {
+        private void Test()
+        {
+            DataBases.Access<object>("test", "testTable").Delete(new Dictionary<string, object>());
+        }
     }
 }
