564190fe61057359f513e71c279578a3040208be
Tristan Taylor Kamp <tris0032@edu.easj.dk>
2020-04-17 12:39:03 +0200

    Vi har lavet en controller til admin, mm.

---
+++ RestAPISCS/Controllers/AdminsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using DBUtility;
+using ModelLibrary;
+using RestAPISCS.App_Start;
+using RestAPISCS.DatabaseUtility;
+
+namespace RestAPISCS.Controllers
+{
+    public class AdminsController : ApiController
+    {
+        private ManageGenericWithLambda<Admin> adminManager = DataBases.Access<Admin>(BaseNames.SikonDatabase, "Admins");
+        private ManageGenericWithLambda<User> userManager = DataBases.Access<User>(BaseNames.SikonDatabase, "UserSikon");
+
+        //The string inside is the name of the primary key for UserSikon table. It is used to associate an actual phone number value you give it ex. "12345678" with the that primary key via its column name, such that it can find a single entry with that phone number.
+        //If you had more than one primary key, you add all of them one by one to the dictionary in the method.
+        public static Dictionary<string, object> PrimaryKeys(string phoneNumber)
+        {
+            Dictionary<string, object> lookupDictionary = new Dictionary<string, object>();
+            lookupDictionary.Add("PhoneNumber", phoneNumber);
+            return lookupDictionary;
+        }
+
+        // GET: api/Admins
+        public IEnumerable<Admin> Get()
+        {
+            return adminManager.Get(Fillables.FillAdmin);
+
+        }
+
+        // GET: api/Admins/5
+        public Admin Get(int id)
+        {
+            string phoneNumber = Convert.ToString(id);
+            return adminManager.GetOne(Fillables.FillAdmin, PrimaryKeys(phoneNumber));
+        }
+
+        // POST: api/Admins
+        public bool Post([FromBody]Admin admin)
+        {
+
+            bool adminOk = adminManager.Post(Extractables.ExtractAdmin(admin));
+
+            bool userOk =
+                userManager.Post(Extractables.ExtractUser(new User(admin.Name, admin.PhoneNumber, admin.Email,
+                    admin.Password)));
+            return userOk && adminOk;
+        }
+
+        // PUT: api/Admins/5
+
+        public bool Put(int id, [FromBody]Admin admin)
+
+        {
+            string phoneNumber = Convert.ToString(id);
+            bool adminOk = adminManager.Put(Extractables.ExtractAdmin(admin), PrimaryKeys(phoneNumber));
+
+            bool userOk =
+                userManager.Put(Extractables.ExtractUser(new User(admin.Name, admin.PhoneNumber, admin.Email,
+                    admin.Password)), PrimaryKeys(phoneNumber));
+            return userOk && adminOk;
+        }
+
+        // DELETE: api/Admins/5
+        public bool Delete(int id)
+        {
+            string phoneNumber = Convert.ToString(id);
+            bool adminDelete = adminManager.Delete(PrimaryKeys(phoneNumber));
+            bool userDelete = userManager.Delete(PrimaryKeys(phoneNumber));
+            return adminDelete && userDelete;
+        }
+    }
+}
--- RestAPISCS/DatabaseUtility/Fillables.cs
+++ RestAPISCS/DatabaseUtility/Fillables.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Dynamic;
 using System.Linq;
 using System.Web;
+using DBUtility;
 using ModelLibrary;
+using RestAPISCS.App_Start;
+using RestAPISCS.Controllers;
 
 namespace RestAPISCS.DatabaseUtility
 {
@@ -15,6 +19,19 @@ namespace RestAPISCS.DatabaseUtility
             user.Name = reader.GetString(reader.GetOrdinal("Name"));
             user.Email = reader.GetString(reader.GetOrdinal("Email"));
             user.Password = reader.GetString(reader.GetOrdinal("Password"));
+        }
+
+        public static void FillAdmin(Admin admin, SqlDataReader reader)
+        {
+            string PhoneNumber =  reader.GetString(reader.GetOrdinal("PhoneNumber"));
+
+            admin.PhoneNumber = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+            admin.AccessLevel = reader.GetInt32(reader.GetOrdinal("AccessLevel"));
+            User placeholder = DataBases.Access<User>(BaseNames.SikonDatabase, "UserSikon")
+                .GetOne(FillUser, UsersController.PrimaryKeys(PhoneNumber));
+            admin.Name = placeholder.Name;
+            admin.Email = placeholder.Email;
+            admin.Password = placeholder.Password;
         }
     }
 }
