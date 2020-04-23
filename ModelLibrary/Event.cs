b5d8a57f3aa343cd5ba64128b83177e0c5d333de
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-04-23 13:44:22 +0200

    Booking, Event og Room

---
+++ ModelLibrary/Event.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLibrary
+{
+    public class Event
+    {
+        public Room Room;
+
+        public DateTime Date { get; set; }
+        public TimeSpan Duration { get; set; }
+        public float Rating { get; set; }
+        public string Abstract { get; set; }
+        public int EventID { get; set; }
+        public int RoomNr { get; set; }
+
+        public Event(DateTime date, TimeSpan duration, float rating, string @abstract, int eventId, int roomNr)
+        {
+            Date = date;
+            Duration = duration;
+            Rating = rating;
+            Abstract = @abstract;
+            EventID = eventId;
+            RoomNr = roomNr;
+        }
+
+        public Event()
+        {
+            
+        }
+    }
+}
---
+++ ModelLibrary/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLibrary
+{
+    public class Room
+    {
+        public int RoomNr { get; set; }
+        public int RoomMaxPersons { get; set; }
+        public int AutistSeats { get; set; }
+
+        public Room(int roomNr, int roomMaxPersons, int autistSeats)
+        {
+            RoomNr = roomNr;
+            RoomMaxPersons = roomMaxPersons;
+            AutistSeats = autistSeats;
+        }
+
+        public Room()
+        {
+            
+        }
+    }
+}
