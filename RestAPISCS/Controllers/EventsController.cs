c22ba845bfc344ae283353c476d6ca9dcbec34b7
Arsen Niyonzima <arse0012@edu.easj.dk>
2020-04-23 15:39:29 +0200

    EventsController

---
+++ RestAPISCS/Controllers/EventsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBUtility;
+using ModelLibrary;
+using RestAPISCS.App_Start;
+using RestAPISCS.DatabaseUtility;
+
+namespace RestAPISCS.Controllers
+{
+    public class EventsController : ApiController
+    {
+        
+
+        private static Dictionary<string, object> PrimaryKeys(int eventId)
+        {
+            Dictionary<string, object> lookupDictionary = new Dictionary<string, object>();
+            lookupDictionary.Add("EventId", eventId);
+            return lookupDictionary;
+        }
+
+        // GET: api/Events
+        //public IEnumerable<Event> Get()
+        //{
+        //    return;
+        //}
+
+        // GET: api/Events/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Events
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Events/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Events/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
