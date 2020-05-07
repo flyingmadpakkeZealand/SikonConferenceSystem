2da344b2cf34c185bcdd884f14d162d8321e8385
Magnus Bahn Berger <magn351i@edu.easj.dk>
2020-05-07 21:53:40 +0200

    Added Converter NameSpace for Xaml Converters. Added BoolToVisibility Converter. Added EventsHandler. Redone EventsPage. Renamed GroupingEventVM to EventsPageVM.

--- SikonConferenceSystem/View/EventsPage.xaml.cs
+++ SikonConferenceSystem/View/EventsPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using ModelLibrary;
+using SikonConferenceSystem.ViewModel;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -31,6 +32,15 @@ namespace SikonConferenceSystem.View
             MaxHeightForGrid = MainPage.AproxFrameHeight;
             this.InitializeComponent();
             //TestCVS.Source = CreateGroups2();
+        }
+
+        //Formatting Work around... Not sure about it, but should be fine...
+        private void DurationTxt_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+
+            if (textBlock.DataContext is EventAdapter @event)
+                textBlock.Text = @event.FormattedDuration();
         }
     }
 }
---
+++ SikonConferenceSystem/ViewModel/EventsPageVM.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using ModelLibrary;
+using SikonConferenceSystem.Annotations;
+using SikonConferenceSystem.Handler;
+using SikonConferenceSystem.Model;
+
+namespace SikonConferenceSystem.ViewModel
+{
+    public class EventsPageVM : INotifyPropertyChanged
+    {
+        private CatalogSingleton<Event> _eventSingleton;
+
+        public CatalogSingleton<Event> EventSingleton
+        {
+            get { return _eventSingleton; }
+        }
+
+        private EventsHandler _handler;
+
+        public EventsHandler Handler
+        {
+            get { return _handler; }
+        }
+
+        private bool _listIsLoading;
+        public bool ListIsLoading
+        {
+            get { return _listIsLoading; }
+            set
+            {
+                _listIsLoading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<HourGroup> _hourGroups;
+
+        public ObservableCollection<HourGroup> HourGroups
+        {
+            get { return _hourGroups; }
+            set
+            {
+                _hourGroups = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> EventDays { get; set; }
+
+        private List<ObservableCollection<HourGroup>> _HourGroupsByDate;
+
+        private int _selectedDayIndex;
+
+        public int SelectedDayIndex
+        {
+            get { return _selectedDayIndex; }
+            set
+            {
+                HourGroups = _HourGroupsByDate[value];
+                _selectedDayIndex = value;
+            }
+        }
+
+
+        private int _hourGroupIterations;
+        public EventsPageVM()
+        {
+            _hourGroupIterations = 24;
+            #region Old Mockup Code
+            //HourGroups = new ObservableCollection<HourGroup>();
+
+            //List<EventAdapter> allEvents = new List<EventAdapter>();
+
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(11), TimeSpan.FromHours(1), 5, "The first event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(11), TimeSpan.FromHours(2), 5, "The second event", 0, 0, "", null, Event.EventType.BigEvent),
+            //    "#33EA1616"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(12), TimeSpan.FromHours(1), 5, "The Third event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(12), TimeSpan.FromHours(1), 5, "The Fourth event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(13), TimeSpan.FromHours(2), 5, "The Fifth event", 0, 0, "", null, Event.EventType.BigEvent),
+            //    "#33EA1616"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(13), TimeSpan.FromHours(1), 5, "The Sixth event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(14), TimeSpan.FromHours(1), 5, "The Seventh event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(14), TimeSpan.FromHours(1), 5, "The Eight event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010"));
+            //allEvents.Add(new EventAdapter(new Event(DateTime.Today.AddHours(13.5), TimeSpan.FromHours(1), 5, "The Test event", 0, 0, "", null, Event.EventType.Workshop),
+            //    "#33DEF010")); 
+            #endregion
+
+            _HourGroupsByDate = new List<ObservableCollection<HourGroup>>();
+            EventDays = new ObservableCollection<string>();
+
+            _handler = new EventsHandler(this);
+
+            _eventSingleton = CatalogSingleton<Event>.Instance;
+            if (_eventSingleton.IsLoading)
+            {
+                ListIsLoading = true;
+                _eventSingleton.Subscribe(SetupEventsList);
+            }
+            else
+            {
+                ListIsLoading = false;
+                SetupEventsList();
+            }
+
+
+        }
+
+        private void SetupEventsList()
+        {
+            var quary = from @event in EventSingleton.Catalog
+                group @event by @event.Date.ToString("dd/MM/yyyy")
+                into eventByDate
+                orderby eventByDate.Key
+                select eventByDate;
+
+            foreach (var eventGroup in quary)
+            {
+                Event eventSample = eventGroup.First();
+                if (eventSample == null)
+                {
+                    throw new ArgumentException("Empty Groups");
+                }
+
+                ObservableCollection<HourGroup> hourGroupsThisDay = new ObservableCollection<HourGroup>();
+                SetupHourGroups(hourGroupsThisDay);
+                AddEvents(eventGroup, hourGroupsThisDay);
+
+                _HourGroupsByDate.Add(hourGroupsThisDay);
+
+                EventDays.Add(eventSample.Date.ToString("dd/MM dddd"));
+            }
+
+            SelectedDayIndex = 0;
+            ListIsLoading = false;
+        }
+
+        private void SetupHourGroups(ObservableCollection<HourGroup> collection)
+        {
+            for (int i = 0; i < _hourGroupIterations; i++)
+            {
+                collection.Add(new HourGroup(TimeSpan.FromHours(i)));
+            }
+        }
+
+        private void AddEvents(IEnumerable<Event> events, ObservableCollection<HourGroup> hourGroupsThisDay)
+        {
+            List<EventAdapter> adaptedEvents = AdaptEvents(events);
+
+            foreach (HourGroup hourGroup in hourGroupsThisDay)
+            {
+                foreach (EventAdapter @event in adaptedEvents)
+                {
+                    if (AddEventCondition(@event, hourGroup))
+                    {
+                        hourGroup.Events.Add(@event);
+                    }
+                }
+            }
+        }
+
+        private List<EventAdapter> AdaptEvents(IEnumerable<Event> events)
+        {
+            List<EventAdapter> adaptedEvents = new List<EventAdapter>();
+
+            foreach (Event @event in events)
+            {
+                adaptedEvents.Add(new EventAdapter(@event));
+            }
+
+            return adaptedEvents;
+        }
+
+        private bool AddEventCondition(EventAdapter @event, HourGroup hourGroup)
+        {
+            bool startingTime = @event.Event.Date.TimeOfDay >= hourGroup.TimeAsTimeSpan &&
+                                @event.Event.Date.TimeOfDay < hourGroup.TimeAsTimeSpan.Add(TimeSpan.FromHours(1));
+
+            bool overlapTime = @event.Event.Date.TimeOfDay.Add(@event.Event.Duration) > hourGroup.TimeAsTimeSpan &&
+                               hourGroup.TimeAsTimeSpan > @event.Event.Date.TimeOfDay;
+
+            return startingTime || overlapTime;
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class HourGroup
+    {
+        public ObservableCollection<EventAdapter> Events { get; set; }
+
+        public HourGroup(TimeSpan time)
+        {
+            Events = new ObservableCollection<EventAdapter>();
+            TimeAsTimeSpan = time;
+            Time = $"{time:hh\\:mm} : {time.Add(TimeSpan.FromHours(1)):hh\\:mm}";
+        }
+
+        public string Time { get; set; }
+
+        public TimeSpan TimeAsTimeSpan { get; set; }
+    }
+
+    public class EventAdapter
+    {
+        private const string WorkshopColor = "#4CE8C20F";
+        private const string MediumEventColor = "#4C434032";
+        private const string BigEventColor = "#4C0043FF";
+
+        public Event Event { get; set; }
+        public string Color { get; set; }
+        public string FormattedDescription { get; set; }
+
+        private bool _formatOccurred;
+
+        public EventAdapter(Event @event)
+        {
+            Event = @event;
+            FormattedDescription = FormatAbstract(@event.Abstract);
+            switch (@event.Type)
+            {
+                case Event.EventType.Workshop:
+                {
+                    Color = WorkshopColor;
+                } break;
+                case Event.EventType.MediumEvent:
+                {
+                    Color = MediumEventColor;
+                } break;
+                case Event.EventType.BigEvent:
+                {
+                    Color = BigEventColor;
+                } break;
+            }
+        }
+
+        private string FormatAbstract(string eventAbstract)
+        {
+            string[] splitAbstract = eventAbstract.Split(';', 2);
+            string pureAbstract = splitAbstract[1];
+            int headerLength = Convert.ToInt32(splitAbstract[0]);
+
+            return pureAbstract.Substring(0, headerLength);
+        }
+
+        public string FormattedDuration()
+        {
+            string result;
+            if (!_formatOccurred)
+            {
+                result = $"Starts this hour! At {Event.Date.TimeOfDay:hh\\:mm}\nDuration : {Event.Duration:hh\\:mm}";
+            }
+            else
+            {
+                result = $"This event started at {Event.Date.TimeOfDay:hh\\:mm}\nDuration : {Event.Duration:hh\\:mm}";
+            }
+
+            _formatOccurred = true;
+            return result;
+        }
+    }
+}
